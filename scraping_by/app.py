0072836e7d2a4dc4501d9a2f971b64abe86537d8
Caitlin <caitlinwhite@gmail.com>
2019-12-15 22:04:41 +1100

    New route to scrape individual products

--- scraping_by/app.py
+++ scraping_by/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from scraping_by import PRODUCTS, scrape
+from scraping_by import PRODUCTS, composeAndPostMessage, scrape
 
 app = Flask(__name__)
 
@@ -11,8 +11,20 @@ def hello():
 
 
 @app.route("/scrape")
-def scrape_products():
+def scrape_all():
     return scrape(PRODUCTS)
+
+
+@app.route("/scrape/<id_>")
+def scrape_by_id(id_):
+    try:
+        index = PRODUCTS.index(int(id_))
+        print(index)
+        product = PRODUCTS[index]
+        composeAndPostMessage(product)
+        return "🤑 Scraped product {}".format(product)
+    except Exception as e:
+        return "🤑" + str(e)
 
 
 if __name__ == "__main__":
